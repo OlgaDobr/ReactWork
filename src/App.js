ef8727cb9b0fa0513bba66392c05b57c4e14e13b
Ольга <olga@MacBook-Air-Olga.local>
2025-06-18 00:29:00 +0300

    menu

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import FastFoodKiosk from './conteiner/Components/FastFoodKiosk';
 import Menu from './conteiner/Components/Menu';
 import Header from './conteiner/Components/Header';
+import React from 'react';
 
 
 
-function App() {
+   function App() {
   return (
     <div className="App">
        <FastFoodKiosk/> 
@@ -14,8 +15,8 @@ function App() {
       <Header/>
 
     </div>
-   
-  );
-}
+);
+};
+
 
 export default App;
